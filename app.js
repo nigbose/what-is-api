47b9c6f6e4c1c3991b80fcb01248731b0679d0d5
Tom Roberts <stty1244@kcl.ac.uk>
2022-11-30 19:21:30 +0000

    do it

---
+++ app.js
@@ -0,0 +1,20 @@
+//https://www.geeksforgeeks.org/how-to-create-and-run-node-js-project-in-vs-code-editor/
+// Requiring module
+const express = require('express');
+ 
+// Creating express object
+const app = express();
+ 
+// Handling GET request
+app.get('/', (req, res) => {
+    res.send('A simple Node App is '
+        + 'running on this server')
+    res.end()
+})
+ 
+// Port Number
+const PORT = process.env.PORT ||5000;
+ 
+// Server Setup
+app.listen(PORT,console.log(
+  `Server started on port ${PORT}`));
---
+++ index_01_self_made_api.js
@@ -0,0 +1,17 @@
+//Initial - version 1
+/*
+const message="Hello World!";
+
+function reverse(msg){
+    message.split('').reverse().join;
+}
+
+reverse()
+*/
+
+//8 mins in - take and import utils.js in
+const utils = require("./utils");
+// so have created a method with a api - .reverse
+//  api - created in code that you write.
+utils.reverse
+
